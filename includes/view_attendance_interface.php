ccbd4168b9daa1864f34a82bb53017c97dd07efe
iamcdr <mafia.skyrim@gmail.com>
2017-09-15 14:44:47 +0800

    attendance-fix

--- includes/view_attendance_interface.php
+++ includes/view_attendance_interface.php
@@ -26,30 +26,32 @@
                         mysqli_query($connection, "INSERT INTO attendance_log(student_id, date_attended, time_in, sent) VALUES ({$row[0]}, '{$date_attended}', '{$time_in}', 1 )");
 
                         $message = "Date: $date_attended \n Time In: $time_in ";
-                        itexmo($contactno, $message);
+                        //itexmo($contactno, $message);
                         $i=1;
                     }
 
                 ?>
 
                 <div class="row">
-                    <div class="col-lg-4">
-                        <label>Full Name</label>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <label>Full Name</label>
+                        </div>
+                        <div class="col-lg-8">
+                            <?= $row['last_name'] . " " . $row['first_name']  ?>
+                        </div>
                     </div>
-                    <div class="col-lg-8">
-                        <?= $row['last_name'] . " " . $row['first_name']  ?>
+                    <div class="row">
+                        <div class="col-lg-4"><label>Birthdate</label></div>
+                        <div class="col-lg-8">
+                            <?= $row['birthdate'] ?>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-4"><label>Birthdate</label></div>
-                    <div class="col-lg-8">
-                        <?= $row['birthdate'] ?>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-4"><label>Section</label></div>
-                    <div class="col-lg-8">
-                        <?= displaySectionDesc($row['section_id']) ?>
+                    <div class="row">
+                        <div class="col-lg-4"><label>Section</label></div>
+                        <div class="col-lg-8">
+                            <?= displaySectionDesc($row['section_id']) ?>
+                        </div>
                     </div>
                 </div>
 
@@ -88,7 +90,7 @@
                         var getStatus = $('#showStatusHere', $(result));
                         $('#showStatusHere').html(getStatus);
 
-                        focusInput(1000);
+                        focusInput(100);
                     }
                 })
             }
